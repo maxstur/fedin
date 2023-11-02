0ff43682f14e0ae00ac00aeadba5b1ab8d89e24a
Maximiliano Rodríguez <maarashu@gmail.com>
2023-11-02 16:57:33 -0300

    inicio

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBarComponent from './components/NavBarComponent/NavBarComponent';
+
+function App() {
+  return (
+    <div>
+      <NavBarComponent/>
+    </div>
+  )   
+}
+export default App 
